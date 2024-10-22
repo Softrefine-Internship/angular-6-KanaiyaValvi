5a55996e85680a6276348fccd71e7b768bbb3e52
valvikanaiya <kanaiya.softrefine@gmail.com>
2024-10-22 13:27:36 +0530

    complete test 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-6';
+export class AppComponent implements OnInit {
+  registrationForm!: FormGroup;
+  countries: { value: string; label: string }[] = [
+    { value: 'ind', label: 'India' },
+    { value: 'usa', label: 'USA' },
+    { value: 'eng', label: 'England' },
+  ];
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      firstName: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      lastName: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+      ]),
+      gender: new FormControl(null, Validators.required),
+      married: new FormControl(false),
+      country: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
+  onClear() {
+    this.registrationForm.reset();
+  }
 }
